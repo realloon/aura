5722f76cb16dd99b66285df57e30a0314c6c2f62
sincerajn <sincerajn@outlook.com>
2026-07-20 11:55:35 +0800

    chore: remove obsolete test file

--- main.ts
+++
@@ -1,8 +0,0 @@
-import { Aura, csharp } from 'aura'
-
-const aura = new Aura().register([csharp])
-const stream = aura.createHighlighter('csharp')
-
-console.log(stream.write('public sealed cla'))
-console.log(stream.write('ss Example {}'))
-console.log(stream.finish())
